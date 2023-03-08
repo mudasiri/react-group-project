f9b2ee70883486683d286c632e1f66b87c5a13dc
Mudasir Issah <danladi496@gmail.com>
2023-03-08 03:01:13 +0000

    Navbar-completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,15 +3,22 @@ import { Link, NavLink } from "react-router-dom";
 
 function Navbar(){
     return(
-        <nav className="nav-bar">
-            <Link to="/" className="nav-brand"><img src="planet.png"/> </Link>
+       <div className="container">
+         <nav className="nav-bar">
+            <div className="nav-brand">
+            <Link to="/" className="nav-brand"><img src="planet.png"/></Link>
+            <h2>Space Travelers' Hub </h2>
+            </div>
        <ul className="nav-links">
         <li><NavLink to="/" className={({ isActive }) => (isActive ? 'active-link nav-link' : 'nav-link')}>Rockets</NavLink></li>
         <li><NavLink to="/categories" className={({ isActive }) => (isActive ? 'active-link nav-link' : 'nav-link')}>Missions</NavLink></li>
-        <div>|</div>
+        <span>|</span>
         <li><NavLink to="/categories" className={({ isActive }) => (isActive ? 'active-link nav-link' : 'nav-link')}>My Profile</NavLink></li>
       </ul>
         </nav>
+        <hr className="horizontal"></hr>
+       </div>
+        
     )
 }
 
---
+++ src/components/rocket.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Rocket(){
+    return(
+        <div className="rocket-info-container">
+            <div className="rocket-img">
+                <img src="https://i.imgur.com/azYafd8.jpg"></img>
+            </div>
+            <div className="rocket-info">
+                <h2>Falcon 1</h2>
+                <p>lorem Ipsum</p>
+                <button type="button"></button>
+            </div>
+        </div>
+    )
+}
