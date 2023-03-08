8d2f9b8dd062233e6e60b0564703857176241a93
Mudasir Issah <danladi496@gmail.com>
2023-03-08 03:23:38 +0000

    Added-rocket-component

--- src/components/rocket.js
+++ src/components/rocket.js
@@ -8,9 +8,11 @@ function Rocket(){
             </div>
             <div className="rocket-info">
                 <h2>Falcon 1</h2>
-                <p>lorem Ipsum</p>
-                <button type="button"></button>
+                <p>The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.</p>
+                <button type="button" className="reserve-btn">Reserve Rocket</button>
             </div>
         </div>
     )
 }
+
+export default Rocket;
--- src/components/rockets.js
+++ src/components/rockets.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Rocket from "./rocket";
 
 function Rockets(){
     return(
-        <>
-        
-        </>
+        <div className="container">
+            <Rocket />
+        </div>
     )
 }
 
